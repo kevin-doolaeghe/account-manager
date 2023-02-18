678dfddd04264b221c01ab37966b07c82526e0b2
Kevin Doolaeghe <doolaeghe.kevin@hotmail.com>
2023-02-18 20:03:31 +0100

    Updated README. Updated Records.

--- README.md
+++ README.md
@@ -11,3 +11,8 @@
 ```
 docker-compose up -d
 ```
+
+## References
+
+* [Entity Framework Core - Relationships](https://learn.microsoft.com/fr-fr/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key)
+* [Razor - Auto generated pages](https://learn.microsoft.com/fr-fr/aspnet/core/tutorials/razor-pages/page?view=aspnetcore-7.0&tabs=visual-studio)
--- frontend/Pages/Privacy.cshtml.cs
+++ frontend/Pages/Privacy.cshtml.cs
@@ -2,14 +2,15 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace frontend.Pages {
+
     public class PrivacyModel : PageModel {
+
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger) {
             _logger = logger;
         }
 
-        public void OnGet() {
-        }
+        public void OnGet() {}
     }
 }
